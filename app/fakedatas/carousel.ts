325679b33418d19ace7eb90c3b8f15aa81a41290
lipeRefosco] <luisfelipe.refosco@hotmail.com>
2023-10-30 02:28:37 -0300

    chore: mockig the Carousel and ProductCard to a better preview of the project

--- app/fakedatas/carousel.ts
+++ app/fakedatas/carousel.ts
@@ -1,10 +1,12 @@
 import { Carousel } from "../types/Carousel";
+import bannerModel from "../assets/BannerModel.jpg"
+
 
 export const carouselData: Carousel[] = Array.from({ length: 5 }, (_, i) => {
     return {
         id: i,
         url_page: `#highlights-${i}`,
-        url_img: `#highlights-${i}`,
+        url_img: bannerModel.src,
         alt_img: `#highlights-${i}`
     }
   });
--- app/fakedatas/products.ts
+++ app/fakedatas/products.ts
@@ -1,10 +1,12 @@
 import { Product } from "../types/Product";
+import tshirtModel from "../assets/tshirt.png"
+
 
 export const products: Product[] = Array.from({length: 5}, (_,i) => {
     return {
         name: `Product name ${i}`,
         previusPrice: 100,
-        newPrice: 90,
-        image: `#product-img-${i}`
+        newPrice: 89.99,
+        image: tshirtModel.src
     }
 })
