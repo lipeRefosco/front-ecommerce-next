c578cb398654d7e2d6a13f3890493156198d2263
lipeRefosco] <luisfelipe.refosco@hotmail.com>
2023-11-13 17:58:18 -0300

    fix[BackButtom/Component]: use the router of the next/navitation to avoid creating a very large history stack

--- app/components/BackButton/index.tsx
+++ app/components/BackButton/index.tsx
@@ -1,19 +1,18 @@
 "use client"
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { TbArrowBackUp } from "react-icons/tb"
 
 import styles from './styles.module.css'
 import Button from "../Button";
 
 export default function BackButton() {
-
+	const router = useRouter()
 	const pathname = usePathname()
 	const onHomePage = pathname === '/' || pathname === ''
 
 	return onHomePage
 		? ''
-		: <Link href={'../'} className={styles.back_btn}>
+		: <div onClick={() => router.back()} className={styles.back_btn}>
 			<Button Icon={TbArrowBackUp} style="" />
-		</Link>
+		</div>
 }
